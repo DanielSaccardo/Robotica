4454dd16ed799eb310088b61a5bcc9bc8485b29d
DanielSaccardo <dsaccardo@chilesotti.it>
2022-02-02 23:35:10 +0100

    Variabili più comprensibili

--- src/defines.h
+++ src/defines.h
@@ -21,12 +21,12 @@
 
 #define MOT_R_FWD                 digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW)
 #define MOT_R_BCK                 digitalWrite(BIN1, LOW); digitalWrite(BIN2, HIGH)
-#define MOT_R_PWM(x,y)            analogWrite(PWM_R, x+y)
+#define MOT_R_PWM(pwm, offset)    analogWrite(PWM_R, pwm + offset)
 
 #define MOT_L_FWD                 digitalWrite(AIN1, HIGH); digitalWrite(AIN2, LOW)
 #define MOT_L_BCK                 digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH)
-#define MOT_L_PWM(x,y)            analogWrite(PWM_L, x+y)
+#define MOT_L_PWM(pwm, offset)    analogWrite(PWM_L, pwm + offset)
 
 #define MOT_ALL_FWD               MOT_L_FWD; MOT_R_FWD
 #define MOT_ALL_BCK               MOT_L_BCK; MOT_R_BCK
-#define MOT_ALL_PWM(x,yR,yL)      MOT_R_PWM(x,yR); MOT_L_PWM(x,yL)
+#define MOT_ALL_PWM(pwm, offset_R, offset_L)      MOT_R_PWM(pwm, offset_R); MOT_L_PWM(pwm, offset_L)
